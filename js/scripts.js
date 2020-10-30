f6f7fffbd61ceb5308b7cecd7782d971c77b9432
Sarah Gilbert <sarahgilbert064@gmail.com>
2020-10-30 16:01:13 -0700

    add lots of commented out code as I am very confused about what to do

--- js/scripts.js
+++ js/scripts.js
@@ -3,35 +3,38 @@
 
 
 
-function mrRoboger(userInputStr) {
+function mrRoboger(userInput) {
   let userInputStr = parseInt(userInputStr);
   let newArray =[];
-  for (let i = 0; index <= )
+  for (let i = 0; i <= userInput; i += 1); {
+    newArray.push(i.toString());
+  }
 }
 
 
 
 
-let mrRoboger = function (inputNumber) {
-  let newArray = [];
-  for (let i = 0; index <=inputNumber; i += 1) {
-    inputNumber.push(i.toString());
-  };
 
-    if (inputNumber.includes(1)) {
-      newArray.push("Beep!");
-    } else if (inputNumber.includes(2)) {
-      newArray.push("Boop!");
-    } else if (inputNumber.includes(3)) {
-      newArray.push("Won't you be my neightbor?");
-    } else {
-      newArray.push(array);
-    }
-  };
-};
+// let mrRoboger = function (inputNumber) {
+//   let newArray = [];
+//   for (let i = 0; index <=inputNumber; i += 1) {
+//     inputNumber.push(i.toString());
+//   };
+
+//     if (inputNumber.includes(1)) {
+//       newArray.push("Beep!");
+//     } else if (inputNumber.includes(2)) {
+//       newArray.push("Boop!");
+//     } else if (inputNumber.includes(3)) {
+//       newArray.push("Won't you be my neightbor?");
+//     } else {
+//       newArray.push(array);
+//     }
+//   };
+// };
 
   
-//UI Logic//
+//User Interface Logic//
 
 $(document).ready(function() {
   $("input#number").submit(function(event) {
